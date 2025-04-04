efcfcdce57bac7f26bc99e9ed09fcccc852ad0d3
ayaka910kudo <aya.caro.brbn8@gmail.com>
2025-04-04 22:43:46 +0900

    モックでAPIを実装した

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,42 @@
 import * as express from "express";
 
-var app = express();
+const app = express();
 
-app.get("/", function (req, res) {
+// ルートAPI
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+// 食べ物一覧取得
+app.get("/api/baby-foods", (req, res) => {
+  const babyFoods = [
+    {
+      id: 1,
+      name: "しらすとわかめのお粥",
+      reactionStars: 3,
+    },
+    {
+      id: 2,
+      name: "やわらか肉じゃが",
+      reactionStars: 2,
+    },
+  ];
+  res.json(babyFoods);
+});
+
+// 食べ物詳細取得
+app.get("/api/baby-foods/:id", (req, res) => {
+  const babyFood = {
+    id: 1,
+    name: "しらすとわかめのお粥",
+    description: "",
+    ingredients: ["米", "わかめ", "醤油"],
+    reactionStars: 3,
+    reactionMemo: "まずまず",
+  };
+  res.json(babyFood);
+});
+
+app.listen(8000, () => {
+  console.log("Example app listening on port 8000!");
 });
