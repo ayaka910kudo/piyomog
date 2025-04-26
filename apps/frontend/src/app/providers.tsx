cfb3d179cb4d42c6705518e97dd56b16a5d7e359
ayaka910kudo <aya.caro.brbn8@gmail.com>
2025-04-26 23:29:44 +0900

    MUIの導入

---
+++ apps/frontend/src/app/providers.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  // ここでテーマのカスタマイズが可能です
+});
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+  );
+}
